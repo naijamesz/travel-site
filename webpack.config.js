ec230485624d1ad9a15633991071a4f78aee2541
naijamesz <kodlikejamesz@hotmail.com>
2023-08-20 04:54:38 +0700

    Completed the Testimonials section

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const path = require("path");
+const path = require('path');
 
 const postCSSPlugins = [
-  require("postcss-import"),
-  require("postcss-mixins"),
-  require("postcss-simple-vars"),
-  require("postcss-nested"),
-  require("autoprefixer"),
+  require('postcss-import'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer'),
 ];
 
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
+  entry: './app/assets/scripts/App.js',
   output: {
-    filename: "bundled.js",
-    path: path.resolve(__dirname, "app"),
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app'),
   },
   devServer: {
-    watchFiles: ["app/**/*.html"],
+    watchFiles: ['app/**/*.html'],
     static: {
-      directory: path.join(__dirname, "app"),
+      directory: path.join(__dirname, 'app'),
       watch: false,
     },
     hot: true,
     port: 3000,
   },
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: [
-          "style-loader",
-          { loader: "css-loader", options: { url: false } },
-          { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } },
+          'style-loader',
+          { loader: 'css-loader', options: { url: false } },
+          { loader: 'postcss-loader', options: { postcssOptions: { plugins: postCSSPlugins } } },
         ],
       },
     ],
