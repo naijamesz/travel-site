319e4b2dc2a3edbd0e4fdda8d0a631d7b32f2fb9
naijamesz <kodlikejamesz@hotmail.com>
2023-08-20 12:17:22 +0700

    Completed large hero style with Grid

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,20 @@
-import "../styles/styles.css";
+import '../styles/styles.css';
 
 if (module.hot) {
   module.hot.accept();
 }
+
+/* Lesson example code below this line */
+// function Person(fullName, favColor) {
+//   this.name = fullName;
+//   this.favoriteColor = favColor;
+//   this.greet = function () {
+//     console.log('Hello, my name is ' + this.name + ' and my favorite color is ' + this.favoriteColor + '.');
+//   };
+// }
+
+// let john = new Person('John Doe', 'blue');
+// john.greet();
+
+// let jane = new Person('Jane Smith', 'green');
+// jane.greet();
